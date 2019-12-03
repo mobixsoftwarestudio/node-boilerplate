0d3cdbc17520759df4d79fb6be75de5e2c798cf2
ytalokaio <ytalo.martins@outlook.com>
2019-12-03 14:54:12 -0300

    FIrst commit boilerplate

---
+++ src/app.ts
@@ -0,0 +1,78 @@
+import express from "express";
+import compression from "compression";  // compresses requests
+import session from "express-session";
+import bodyParser from "body-parser";
+import lusca from "lusca";
+import mongo from "connect-mongo";
+import flash from "express-flash";
+import path from "path";
+import mongoose from "mongoose";
+import passport from "passport";
+import bluebird from "bluebird";
+import { MONGODB_URI, SESSION_SECRET } from "./util/secrets";
+import routes from "./modules/routes";
+
+const MongoStore = mongo(session);
+
+// Create Express server
+const app = express();
+
+// Connect to MongoDB
+const mongoUrl = MONGODB_URI;
+mongoose.Promise = bluebird;
+
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true } ).then(
+    () => { /** ready to use. The `mongoose.connect()` promise resolves to undefined. */ },
+).catch(err => {
+    console.log("MongoDB connection error. Please make sure MongoDB is running. " + err);
+    // process.exit();
+});
+
+// Express configuration
+app.set("port", process.env.PORT || 3000);
+app.use(compression());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({
+    resave: true,
+    saveUninitialized: true,
+    secret: SESSION_SECRET,
+    store: new MongoStore({
+        url: mongoUrl,
+        autoReconnect: true
+    })
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+app.use(lusca.xframe("SAMEORIGIN"));
+app.use(lusca.xssProtection(true));
+app.use((req, res, next) => {
+    res.locals.user = req.user;
+    next();
+});
+app.use((req, res, next) => {
+    // After successful login, redirect back to the intended page
+    if (!req.user &&
+    req.path !== "/login" &&
+    req.path !== "/signup" &&
+    !req.path.match(/^\/auth/) &&
+    !req.path.match(/\./)) {
+        req.session.returnTo = req.path;
+    } else if (req.user &&
+    req.path == "/account") {
+        req.session.returnTo = req.path;
+    }
+    next();
+});
+
+app.use(
+    express.static(path.join(__dirname, "public"), { maxAge: 31557600000 })
+);
+
+/**
+ * Primary app routes.
+ */
+app.use(routes);
+
+export default app;
